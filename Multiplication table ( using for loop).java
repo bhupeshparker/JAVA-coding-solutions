57cbedd3dff92cf3861a76f98dff48f2a5088b36
bhupeshparker <162616643+bhupeshparker@users.noreply.github.com>
2025-01-27 21:02:25 +0530

    Conditional and Loops Q17
    Multiplication table ( using for loop)

---
+++ Multiplication table ( using for loop).java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Main
+{
+  public static void main(String[] args)
+  {
+      Scanner sc=new Scanner(System.in);
+      int n= sc.nextInt();
+      int m= sc.nextInt();
+      for(int i=1; i<=m; i++){
+          System.out.println(n*i);
+      }
+      sc.close();
+  }
+}
